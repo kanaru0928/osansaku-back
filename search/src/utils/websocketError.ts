748a247c39d625513dfb9827ca484ebbc3f60228
kanaru0928 <kanaru0928@gmail.com>
2023-08-21 09:38:37 +0000

    websocketerrorを更新

--- search/src/utils/websocketError.ts
+++ search/src/utils/websocketError.ts
@@ -1,5 +1,10 @@
 export const WebsocketError = {
   USER_NOT_FOUND: 'USER_NOT_FOUND',
+  INVALID_REQUEST: 'INVALID_REQUEST',
+  NO_SETTINGS: 'NO_SETTINGS',
+  UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+  ROUTE_NOT_FOUND: 'ROUTE_NOT_FOUND',
 };
 
-export type WebsocketError = (typeof WebsocketError)[keyof typeof WebsocketError]
+export type WebsocketError =
+  (typeof WebsocketError)[keyof typeof WebsocketError];
