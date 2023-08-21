31df847c0a69b55e01f6a1560a353c5a7f60e53c
kanaru0928 <kanaru0928@gmail.com>
2023-08-21 02:29:05 +0000

    WebSocketErrorを更新

--- search/src/utils/websocketError.ts
+++ search/src/utils/websocketError.ts
@@ -1,3 +1,5 @@
-const WebsocketError = {
+export const WebsocketError = {
   USER_NOT_FOUND: 'USER_NOT_FOUND',
 };
+
+export type WebsocketError = (typeof WebsocketError)[keyof typeof WebsocketError]
